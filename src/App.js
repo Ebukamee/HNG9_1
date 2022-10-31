17c5423cd8dcf8d85987991246937a0c31acf7cf
Ebuka <99645482+Ebukamee@users.noreply.github.com>
2022-11-01 00:01:55 +0100

    Update2

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ function App() {
   return (
     <div className="App">
         <div class="profile">
-    <div class="me"><img class="pp" src="me.png"/></div>
+    <div className="me"><img class="pp" src="me.png"/></div>
     <h3>Nwokike Chukwuebuka</h3>
   </div>
-  <div class="links">
+  <div className="links">
     <a href="https://hng-9-1.vercel.app/Twitter Link"</a>
     <a href="https://hng-9-1.vercel.app/Zuri Team"</a>
     <a href="https://hng-9-1.vercel.app/Zuri Books"</a>
     <a href="https://hng-9-1.vercel.app/Python Books/python-for-beginers?ref_id"</a>
     <a href="">Background Check for Books</a>
     <a href="">Design Books</a>
-    <div class="image">
-      <a href="" class="img"><img src="slack.png" /></a>
-       <a href="" class="img"><img src="github.png" /></a>
+    <div className="image">
+      <a href="" className="img"><img src="slack.png" /></a>
+       <a href="" className="img"><img src="github.png" /></a>
     </div>
   </div><hr />
   <footer>
-    <img class="zuri" src="zuri.png">
+    <img className="zuri" src="zuri.png">
     <p>HNG Internship 9 Frontend Task</p>
-     <img class="zuri" src="ig.png">
+     <img className="zuri" src="ig.png">
   </footer>
     </div>
   );
