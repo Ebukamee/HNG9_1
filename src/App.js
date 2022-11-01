d627ea60351415c673f17da53551bca7ef8e4e68
Ebuka <99645482+Ebukamee@users.noreply.github.com>
2022-11-01 09:06:02 +0100

    My change

--- src/App.js
+++ src/App.js
@@ -4,26 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <div class="profile">
-    <div className="me"><img class="pp" src="me.png"/></div>
+     <div class="profile">
+    <div class="me"><img class="pp" src="me.png"/></div>
     <h3>Nwokike Chukwuebuka</h3>
   </div>
-  <div className="links">
-    <a href="https://hng-9-1.vercel.app/Twitter Link"</a>
-    <a href="https://hng-9-1.vercel.app/Zuri Team"</a>
-    <a href="https://hng-9-1.vercel.app/Zuri Books"</a>
-    <a href="https://hng-9-1.vercel.app/Python Books/python-for-beginers?ref_id"</a>
+  <div class="links">
+    <a href="">Twitter Link</a>
+    <a href="https://hng-9-1.vercel.app/Zuri Team">Zuri Team</a>
+    <a href="https://hng-9-1.vercel.app/Zuri Books">Zuri Books</a>
+    <a href="">Python Books</a>
     <a href="">Background Check for Books</a>
     <a href="">Design Books</a>
-    <div className="image">
-      <a href="" className="img"><img src="slack.png" /></a>
-       <a href="" className="img"><img src="github.png" /></a>
+    <div class="image">
+      <a href="" class="img"><img src="slack.png" /></a>
+       <a href="" class="img"><img src="github.png" /></a>
     </div>
   </div><hr />
   <footer>
-    <img className="zuri" src="zuri.png">
+    <img class="zuri" src="zuri.png">
     <p>HNG Internship 9 Frontend Task</p>
-     <img className="zuri" src="ig.png">
+     <img class="zuri" src="ig.png">
   </footer>
     </div>
   );
